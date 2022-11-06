01ae0dd37af5beb22c9335a9433101d1564e9f6e
Daniel Tomala <danieltomala01@gmail.com>
2022-11-06 18:25:06 +0100

    Initialized javafx app

---
+++ src/main/java/runner/HelloController.java
@@ -0,0 +1,14 @@
+package runner;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
