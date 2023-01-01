cc5e51d780964a7fc48c992c06ae64b38a883009
Daniel Tomala <danieltomala01@gmail.com>
2023-01-01 18:55:01 +0100

    Added formation change in SquadView

--- src/main/java/com/example/view/list/SquadView.java
+++ src/main/java/com/example/view/list/SquadView.java
@@ -1,5 +1,6 @@
 package com.example.view.list;
 
+import com.example.model.Formation;
 import com.example.model.Player;
 import com.example.model.Player.Position;
 import com.vaadin.flow.component.Unit;
@@ -11,11 +12,11 @@ import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.select.Select;
 import com.vaadin.flow.data.renderer.ComponentRenderer;
-import com.vaadin.flow.data.renderer.LitRenderer;
 import com.vaadin.flow.data.renderer.NativeButtonRenderer;
-import com.vaadin.flow.data.renderer.Renderer;
 import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.PreserveOnRefresh;
 import com.vaadin.flow.router.Route;
 
 import java.util.ArrayList;
@@ -23,16 +24,21 @@ import java.util.List;
 
 @Route(value = "squad", layout = AppLayoutBasic.class)
 @PageTitle("Squad")
+@PreserveOnRefresh //Czy to będzie git?
+//TODO zrobić żeby się nie renderowała za każdym razem od nowa
 public class SquadView extends HorizontalLayout {
 
+    private ArrayList<Player> players;
     private ArrayList<Player> firstSquad;
     private ArrayList<Player> substitutes; //Must be mutable list i.e ArrayList
+
+    private Select<Formation> selectFormation = createFormationSelect();
 
     public SquadView() {
         setSizeFull();
         setDefaultVerticalComponentAlignment(Alignment.CENTER);
 //        var players = getSamplePlayers();
-        var players = Player.getAllPlayersFromDB().subList(0, 50);
+        players = new ArrayList<>(Player.getAllPlayersFromDB().subList(0, 50));
         firstSquad = new ArrayList<>(players.subList(0, 11));
         substitutes = new ArrayList<>(players.subList(15, 25));
 
@@ -50,16 +56,18 @@ public class SquadView extends HorizontalLayout {
         vL.getStyle().set("background-repeat", "no-repeat");
         vL.getStyle().set("background-position", "center");
 
-        vL.add(getPlayersLineLayout(getPlayersFromPosition(players, List.of(Position.LF, Position.CF, Position.RF)), 2));
-        vL.add(getPlayersLineLayout(getPlayersFromPosition(players, List.of(Position.LM, Position.CM, Position.RM)), 4));
-        vL.add(getPlayersLineLayout(getPlayersFromPosition(players, List.of(Position.LB, Position.CB, Position.RB)), 4));
+        Formation formation = selectFormation.getValue();
+
+        vL.add(getPlayersLineLayout(getPlayersFromPosition(players, List.of(Position.LF, Position.CF, Position.RF)), formation.getForwardsNumber()));
+        vL.add(getPlayersLineLayout(getPlayersFromPosition(players, List.of(Position.LM, Position.CM, Position.RM)), formation.getMidfieldersNumber()));
+        vL.add(getPlayersLineLayout(getPlayersFromPosition(players, List.of(Position.LB, Position.CB, Position.RB)), formation.getDefendersNumber()));
         vL.add(getPlayersLineLayout(getPlayersFromPosition(players, List.of(Position.GK)), 1));
 
         return vL;
     }
 
     private List<Player> getPlayersFromPosition(List<Player> players, List<Position> positions) {
-        return players.stream()
+        return players.stream() //TODO wybierać dobrych zawodników
                 .filter(p -> positions.contains(p.getPosition()))
                 .toList();
     }
@@ -78,12 +86,12 @@ public class SquadView extends HorizontalLayout {
         return vL;
     }
 
-    private HorizontalLayout getPlayersLineLayout(List<Player> players, int players_no) {
+    private HorizontalLayout getPlayersLineLayout(List<Player> players, int playersNo) {
         HorizontalLayout horizontalLayout = new HorizontalLayout();
 //        for (var player : players) {
 //            horizontalLayout.add(getPlayerImageWithSurname(player));
 //        }
-        for (int i = 0; i < players_no; i++) {
+        for (int i = 0; i < playersNo; i++) {
             horizontalLayout.add(getPlayerImageWithSurname(players.get(i)));
         }
         horizontalLayout.getStyle().set("padding-top", "15px");
@@ -121,8 +129,20 @@ public class SquadView extends HorizontalLayout {
                 }));
 
 
-        vL.add(firstSquadGrid, substitutesGrid);
+        vL.add(selectFormation, firstSquadGrid, substitutesGrid);
         return vL;
+    }
+
+    private Select<Formation> createFormationSelect() {
+        Select<Formation> select = new Select<>();
+        select.setLabel("Formation");
+        select.setItemLabelGenerator(Formation::getName);
+        select.setItems(Formation.values());
+        select.setValue(Formation.F_442);
+        select.addValueChangeListener(selectFormationComponentValueChangeEvent -> {
+            this.replace(this.getComponentAt(0), pitchLayout(players));
+        });
+        return select;
     }
 
     private Grid<Player> createPlayersGrid() {
@@ -147,55 +167,32 @@ public class SquadView extends HorizontalLayout {
         return grid;
     }
 
-    private Renderer<Player> createRemoveButton() {
-        return LitRenderer.<Player>of(
-                        "<vaadin-button>Usuń</vaadin-button>")
-                .withFunction("handleClick",
-                        player -> {
-                            substitutes.add(player);
-                            firstSquad.remove(player);
-                        });
-    }
-
-    private Renderer<Player> createAddButton() {
-        return LitRenderer.<Player>of(
-                        "<vaadin-button>Dodaj</vaadin-button>").
-                withFunction("handleClick",
-                        player -> {
-                            if (firstSquad.size() == 11) {
-                                //TODO wyświetlić komunikat
-                            } else {
-                                firstSquad.add(player);
-                                substitutes.remove(player);
-                            }
-                        });
-    }
 
     //FIXME Should be deleted later
     private List<Player> getSamplePlayers() {
-        List<Player> players = new ArrayList<>();
-        players.add(new Player(1, "Wojciech", "Szczęsny", null, null, null, null, null, Position.GK));
-        players.add(new Player(2, "Bartosz", "Bereszyński", null, null, null, null, null, Position.LB));
-        players.add(new Player(3, "Kamil", "Glik", null, null, null, null, null, Position.CB));
-        players.add(new Player(4, "Jakub", "Kiwior", null, null, null, null, null, Position.CB));
-        players.add(new Player(5, "Matty", "Cash", null, null, null, null, null, Position.RB));
-        players.add(new Player(6, "Przemysław", "Frankowski", null, null, null, null, null, Position.RM));
-        players.add(new Player(7, "Krystian", "Bielik", null, null, null, null, null, Position.CM));
-        players.add(new Player(8, "Piotr", "Zieliński", null, null, null, null, null, Position.CM));
-        players.add(new Player(9, "Nicola", "Zalewski", null, null, null, null, null, Position.LM));
-        players.add(new Player(10, "Arkadiusz", "Milik", null, null, null, null, null, Position.CF));
-        players.add(new Player(11, "Robert", "Lewandowski", null, null, null, null, null, Position.CF));
-        players.add(new Player(12, "Łukasz", "Skorupski", null, null, null, null, null, Position.GK));
-        players.add(new Player(13, "Kamil", "Grabara", null, null, null, null, null, Position.GK));
-        players.add(new Player(14, "Grzegorz", "Krychowiak", null, null, null, null, null, Position.CM));
-        players.add(new Player(15, "Artur", "Jędrzejczyk", null, null, null, null, null, Position.CB));
-        players.add(new Player(16, "Jakub", "Kamiński", null, null, null, null, null, Position.RM));
-        players.add(new Player(17, "Michał", "Skóraś", null, null, null, null, null, Position.LM));
-        players.add(new Player(18, "Kamil", "Grosicki", null, null, null, null, null, Position.LM));
-        players.add(new Player(19, "Krzysztof", "Piątek", null, null, null, null, null, Position.CF));
-        players.add(new Player(20, "Karol", "Świderski", null, null, null, null, null, Position.CF));
-        players.add(new Player(21, "Kamil", "Wieteska", null, null, null, null, null, Position.CB));
-        players.add(new Player(22, "Sebastian", "Szymański", null, null, null, null, null, Position.CM));
-        return players;
+        List<Player> samplePlayers = new ArrayList<>();
+        samplePlayers.add(new Player(1, "Wojciech", "Szczęsny", null, null, null, null, null, Position.GK));
+        samplePlayers.add(new Player(2, "Bartosz", "Bereszyński", null, null, null, null, null, Position.LB));
+        samplePlayers.add(new Player(3, "Kamil", "Glik", null, null, null, null, null, Position.CB));
+        samplePlayers.add(new Player(4, "Jakub", "Kiwior", null, null, null, null, null, Position.CB));
+        samplePlayers.add(new Player(5, "Matty", "Cash", null, null, null, null, null, Position.RB));
+        samplePlayers.add(new Player(6, "Przemysław", "Frankowski", null, null, null, null, null, Position.RM));
+        samplePlayers.add(new Player(7, "Krystian", "Bielik", null, null, null, null, null, Position.CM));
+        samplePlayers.add(new Player(8, "Piotr", "Zieliński", null, null, null, null, null, Position.CM));
+        samplePlayers.add(new Player(9, "Nicola", "Zalewski", null, null, null, null, null, Position.LM));
+        samplePlayers.add(new Player(10, "Arkadiusz", "Milik", null, null, null, null, null, Position.CF));
+        samplePlayers.add(new Player(11, "Robert", "Lewandowski", null, null, null, null, null, Position.CF));
+        samplePlayers.add(new Player(12, "Łukasz", "Skorupski", null, null, null, null, null, Position.GK));
+        samplePlayers.add(new Player(13, "Kamil", "Grabara", null, null, null, null, null, Position.GK));
+        samplePlayers.add(new Player(14, "Grzegorz", "Krychowiak", null, null, null, null, null, Position.CM));
+        samplePlayers.add(new Player(15, "Artur", "Jędrzejczyk", null, null, null, null, null, Position.CB));
+        samplePlayers.add(new Player(16, "Jakub", "Kamiński", null, null, null, null, null, Position.RM));
+        samplePlayers.add(new Player(17, "Michał", "Skóraś", null, null, null, null, null, Position.LM));
+        samplePlayers.add(new Player(18, "Kamil", "Grosicki", null, null, null, null, null, Position.LM));
+        samplePlayers.add(new Player(19, "Krzysztof", "Piątek", null, null, null, null, null, Position.CF));
+        samplePlayers.add(new Player(20, "Karol", "Świderski", null, null, null, null, null, Position.CF));
+        samplePlayers.add(new Player(21, "Kamil", "Wieteska", null, null, null, null, null, Position.CB));
+        samplePlayers.add(new Player(22, "Sebastian", "Szymański", null, null, null, null, null, Position.CM));
+        return samplePlayers;
     }
 }
