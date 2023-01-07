f707b126cfb560cfa1d3a37b51af7a2bc3cc31ec
Daniel Tomala <danieltomala01@gmail.com>
2023-01-07 12:21:15 +0100

    Added GameplayView and updated styles

---
+++ src/main/java/com/example/views/GameplayView.java
@@ -0,0 +1,15 @@
+package com.example.views;
+
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@Route(value = "/gameplay", layout = AppLayoutBasic.class)
+@PageTitle("Gameplay")
+public class GameplayView extends HorizontalLayout {
+
+    public GameplayView(){
+
+    }
+
+}
