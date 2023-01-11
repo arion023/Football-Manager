e717ed87c1c0eb552ac70b4bac889604d25c7983
Marcin Kowalczyk <01169191@pw.edu.pl>
2023-01-11 01:22:16 +0100

    add some view and modified generateUser trigger

--- src/main/sql/db-triggers.sql
+++ src/main/sql/db-triggers.sql
@@ -1,7 +1,24 @@
-CREATE VIEW free_players
+CREATE OR REPLACE VIEW default_players
 AS
 SELECT *
-FROM player WHERE player.club_id IN (SELECT club_id FROM club);
+FROM player WHERE player.club_id IN (SELECT club_id FROM users);
+
+CREATE OR REPLACE VIEW default_players
+AS
+SELECT *
+FROM player WHERE player.club_id NOT IN (SELECT club_id FROM club);
+
+
+CREATE OR REPLACE VIEW user_clubs
+AS
+SELECT *
+FROM club WHERE club_id IN (SELECT club_id FROM users);
+
+
+CREATE OR REPLACE VIEW default_clubs
+AS
+SELECT *
+FROM club WHERE club_id NOT IN (SELECT club_id FROM users);
 
 
 --TODO make copy of players
@@ -11,30 +28,31 @@ FOR EACH ROW
 DECLARE
     players_counter NUMBER;
 BEGIN
+    DBMS_SNAPSHOT.REFRESH('default_players');
     -- 2 5 5 3
     --GK
     SELECT count(player_id) INTO players_counter FROM player WHERE club_id = :new.club_id;
     IF players_counter < 11 THEN
 
-    FOR rand_player IN (SELECT * FROM player WHERE POSITION_ID = 1 ORDER BY DBMS_RANDOM.value FETCH NEXT 2 ROWS ONLY)
+    FOR rand_player IN (SELECT * FROM default_players WHERE POSITION_ID = 1 ORDER BY DBMS_RANDOM.value FETCH NEXT 2 ROWS ONLY)
     LOOP
         UPDATE player SET club_id = :new.club_id WHERE player_id = rand_player.PLAYER_ID;
     END LOOP;
 
     --DEFENCE
-    FOR rand_player IN (SELECT * FROM player WHERE POSITION_ID = 2 ORDER BY DBMS_RANDOM.value FETCH NEXT 5 ROWS ONLY)
+    FOR rand_player IN (SELECT * FROM default_players WHERE POSITION_ID = 2 ORDER BY DBMS_RANDOM.value FETCH NEXT 5 ROWS ONLY)
         LOOP
             UPDATE player SET club_id = :new.club_id WHERE player_id = rand_player.PLAYER_ID;
         END LOOP;
 
     --MIDFIELDERS
-    FOR rand_player IN (SELECT * FROM player WHERE POSITION_ID = 3 ORDER BY DBMS_RANDOM.value FETCH NEXT 5 ROWS ONLY)
+    FOR rand_player IN (SELECT * FROM default_players WHERE POSITION_ID = 3 ORDER BY DBMS_RANDOM.value FETCH NEXT 5 ROWS ONLY)
         LOOP
             UPDATE player SET club_id = :new.club_id WHERE player_id = rand_player.PLAYER_ID;
         END LOOP;
 
     --FORWARDS
-    FOR  rand_player IN (SELECT * FROM player WHERE POSITION_ID = 4 ORDER BY DBMS_RANDOM.value FETCH NEXT 3 ROWS ONLY)
+    FOR  rand_player IN (SELECT * FROM default_players WHERE POSITION_ID = 4 ORDER BY DBMS_RANDOM.value FETCH NEXT 3 ROWS ONLY)
         LOOP
             UPDATE player SET club_id = :new.club_id WHERE player_id = rand_player.PLAYER_ID;
         END LOOP;
@@ -42,21 +60,20 @@ BEGIN
     END IF;
 
     --OFFER
-    FOR rand_player IN (SELECT * FROM free_players ORDER BY DBMS_RANDOM.value FETCH NEXT 30 ROWS ONLY)
+    FOR rand_player IN (SELECT * FROM default_players ORDER BY DBMS_RANDOM.value FETCH NEXT 30 ROWS ONLY)
         LOOP
             INSERT INTO offer VALUES(DEFAULT, :new.USER_ID, rand_player.PLAYER_ID, (SELECT TRUNC ((DBMS_RANDOM.NORMAL * 10000) + 50000) to_int FROM DUAL));
         END LOOP;
-
 
 END;
 
 /
 
---INSERT INTO club VALUES (4000, 'Legia Warszawa', 'PL', 106, NULL);
---
---DELETE FROM users WHERE user_id = 100;
---
---INSERT INTO users VALUES (100, 'mail@gmail.com', 'essa', 1000000, 4000, NULL);
+INSERT INTO club VALUES (4000, 'Legia Warszawa', 'PL', 106, NULL);
+
+DELETE FROM users WHERE user_id = 100;
+
+INSERT INTO users VALUES (100, 'mail@gmail.com', 'essa', 1000000, 4000, NULL);
 --
 --SELECT * FROM player WHERE CLUB_ID = 4000;
 --SELECT * FROM offer WHERE user_id = 100;
